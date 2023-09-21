c5f7c75d08d27c8e45f1a17c353785e1f4ef8fa7
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2023-09-21 22:01:19 +0000

    JS dataTypes added

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) //we are using node.js, not browser
+
+let name = "ShuvroWritesCode"
+let age = 22
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
