c4cbba249b4782451bde104697575add63bf5e8d
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2023-09-22 21:52:58 +0000

    JS comparison added

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,27 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+// make sure to avoid these type of data type conflicts in comparison
+console.log("0" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null != 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined != 0);
+
+/* 
+    The == operator performs a loose equality comparison that performs type coercion if necessary to make the comparison possible. The === operator, on the other hand, performs a strict equality comparison that does not perform type coercion and requires the operands to have the same type (as well as the same value).
+*/
+
+console.log("2" === 2);
