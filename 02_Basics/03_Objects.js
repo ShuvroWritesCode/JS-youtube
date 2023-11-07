304abc6ce3cea09e5fae1bc90ae951987a45cb26
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2023-11-07 16:05:42 +0000

    objects p1

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,20 @@
+// singleton
+// Object.create
+
+// objects literals
+const mySym = Symbol("key1")
+
+
+const account = {
+    name: "Shuvro",
+    "full name": "ShuvroWritesCode",
+    age: 19,
+    [mySym]: "mykey1",
+    location: "Dhaka",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "saturday"]
+}
+
+console.log(account.location)
+console.log(account["full name"])
+console.log(account[mySym])
